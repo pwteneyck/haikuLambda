c650db5cd6fe1b0e046fcec0a12cd16b1480d87e
Peter Ten Eyck <peteeyck@amazon.com>
2023-06-14 10:15:38 -0500

    adds a challenge handler

--- haiku.py
+++ haiku.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import os
+import re
 import requests
 from string import punctuation
 
@@ -9,6 +10,20 @@ WORDS_URL = 'https://wordsapiv1.p.rapidapi.com/words/'
 SLACK_URL = 'https://slack.com/api/'
 SLACK_TOKEN = os.environ['slack_token']
 
+# To avoid wordsapi's API limits, cache responses in a DDB table
+#
+# Schema:
+#       HASH_KEY |               | 
+#       ---------|---------------|-----------------------
+#       word (S) | syllables (N) | needs_review (Boolean)
+#
+# word:         the string we requested a syllable count for from wordsapi
+# syllables:    the number of syllables wordsapi says the string has - if 
+#                   wordsapi doesn't know, this defaults to 1
+# needs_review: if wordsapi didn't have a syllable count for this word, this
+#                   gets set to 'true'. Then (ideally) you can go through the
+#                   "needs_review" entries and update their cached syllable
+#                   counts yourself
 ddb_client = boto3.resource('dynamodb', 'us-east-1')
 syllables_cache_table = ddb_client.Table(os.environ['syllables_cache_table'])
 
@@ -22,7 +37,19 @@ def syllables_from_wordsapi(word):
         return None
 
 
+# TODO - ChatGPT seems to do a better job of determining how many syllables are
+# in a word than wordsapi does, but it's much slower. Consider either:
+#   1.  Replacing wordsapi and our custom "haiku-ify" logic with a request to 
+#       ChatGPT along the lines of "is the following string a haiku?"
+#   2.  Use ChatGPT to scan through the "needs_review" entries in the DDB cache
+#       instead of doing them all manually
 def syllables(word):
+    if re.search("^(:|<http)", word):
+        # URLs (starts with '<https') and emojis (starts with ':') should 
+        # disqualify a message from haiku-ification. Disqualify a message by
+        # returning '8', which means the word can never fit into a haiku.
+        return 8
+    word = word.strip(' \'"`_*~/:.!?,;#()@').lower()
     check_cache = syllables_cache_table.get_item(Key={'word':word})
     if 'Item' in check_cache:
         return int(check_cache['Item']['syllables'])
@@ -58,18 +85,22 @@ def send_message(text, channel_id, thread_ts):
         )
         
 def should_ignore(slack_event):
+    # ignore messages sent by bots, including this one
     if 'bot_id' in slack_event:
         return True
+    # ignore messages that don't include any text
     if 'text' not in slack_event:
         return True
+    # ignore things that aren't messages
     if slack_event['type'] != 'message':
         return True
+    # only respond to messages in channels or group DMs
     if not (slack_event['channel_type'] == 'channel' or slack_event['channel_type'] == 'group'):
         return True
     return False
-    
+
 def haiku_ify(input_text):
-    words = input_text.split(' ')
+    words = [x for x in input_text.split() if len(x)>0]
     line_count = 0
     syllable_count = 0
     current_line = ''
@@ -80,7 +111,7 @@ def haiku_ify(input_text):
         else:
             current_line += word
         current_line += ' '
-        syllable_count += syllables(word.strip(punctuation).lower())
+        syllable_count += syllables(word)
         if syllable_count == 5 or syllable_count == 12 or syllable_count == 17:
             output_text += current_line
             output_text += '\n'
@@ -92,8 +123,25 @@ def haiku_ify(input_text):
     else:
         return None
 
+def respond_to_challenge(body):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'challenge': body['challenge']
+        })
+    }
+
 def lambda_handler(event, context):
-    slack_event = json.loads(event['body'])['event']
+    body = json.loads(event['body'])
+    
+    # Don't leave this on by default - otherwise anyone can add this app
+    # whenever to any workspace whenever they want. Instead, just un-comment
+    # it whenever you want to add this app to a new workspace.
+    # TODO figure out OAuth and rate-limiting, etc
+    # if 'challenge' in body:
+    #     return respond_to_challenge(body)
+
+    slack_event = body['event']
     print(slack_event)
     if should_ignore(slack_event):
         return {
@@ -106,6 +154,7 @@ def lambda_handler(event, context):
     
     if haiku:
         print('this is a haiku')
+        # Add markdown block quote syntax to the haiku itself ('>')
         haiku = haiku.strip().replace("\n", "\n>")
         user = user_from_id(slack_event['user'])['profile']['real_name']
         ts = slack_event['ts']
